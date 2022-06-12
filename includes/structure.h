945eae6db5afb7d7b98da267ff1b350248be5def
Omar <omarelghamry@icloud.com>
2022-06-12 04:28:31 +0400

    basic struct is done

---
+++ includes/structure.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structure.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ommohame < ommohame@student.42abudhabi.ae> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/12 01:35:36 by ommohame          #+#    #+#             */
+/*   Updated: 2022/06/12 04:28:03 by ommohame         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURE_H
+# define STRUCTURE_H
+
+/*
+* linked list of every command
+*	- nargs: how many arguments in the line including the function
+*	- args: the arguments stored in double array
+*		---> args[0]: the command
+*	- next: the next command
+*/
+typedef struct s_cmd
+{
+	int				nargs;
+	char			**args;
+	struct s_cmd	*next;
+}	t_cmd;
+
+/*
+* analyzes the line and put it in the struct
+*	- ncmds: how many commands
+*	- nargs: how many arguments in the line including the function
+*	- npipes: how many pipes in the line
+*	- infile: input redirection - NULL if there's no redirection
+*	- outfile: out redirection - NULL if there's no redirection
+*	- errfile: error redirection - NULL if there's no redirection
+*/
+typedef struct s_line
+{
+	int		ncmds;
+	int		nargs;
+	int		npipes;
+	char	*infile;
+	char	*outfile;
+	char	*errfile;
+}	t_line;
+
+#endif
