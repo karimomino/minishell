1c7331fd8523ab9230015db3b17469170963b1c7
Omar <omarelghamry@icloud.com>
2022-07-30 20:45:28 +0400

    before merge

--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -6,7 +6,7 @@
 /*   By: ommohame < ommohame@student.42abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 23:23:04 by ommohame          #+#    #+#             */
-/*   Updated: 2022/07/29 22:04:52 by ommohame         ###   ########.fr       */
+/*   Updated: 2022/07/29 23:05:32 by ommohame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@ int	pipes(t_cmd *cmd)
 {
 	int		pid;
 	int		fd[2];
-	t_cmd *cmd2;
+	(void)cmd;
 	// int		tmpin;
 	// int		tmpout;
 
-	pid = fork();
-	cmd2 = cmd->next;
 	if (pipe(fd) == -1)
 	{
 		ft_printf("minishell: pipe failed m3lsh\n");
 		return (-1);
 	}
+	pid = fork();
 	if (pid == -1)
 	{
 		ft_printf("minishell: man down, fork is not working\n");
@@ -34,17 +33,17 @@ int	pipes(t_cmd *cmd)
 	}
 	else if (pid == 0)
 	{
-		dup2(fd[1], 1);
-		close(fd[0]);
-		exec_ft(cmd2);
-		exit(0);
+		dup2(fd[0], 0);
+		close(fd[1]);
+		char *grep_args[] = {"grep", "test", NULL};
+		execvp("grep", grep_args);
 	}
 	else
 	{
-		dup2(fd[0], 0);
-		close(fd[1]);
 		dup2(fd[1], 1);
-		exec_ft(cmd);
+		close(fd[0]);
+		char *grep_args[] = {"ls", "-la", NULL};
+		execvp("ls", grep_args);
 	}
 	return (1);
 }
