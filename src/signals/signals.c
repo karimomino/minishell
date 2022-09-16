9f1ef131c8a2c3810ce45470c07e6210e59ecf5f
Omar <omarelghamry@icloud.com>
2022-09-16 05:13:30 +0400

    final final final final merge done

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: ommohame < ommohame@student.42abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 01:47:45 by ommohame          #+#    #+#             */
-/*   Updated: 2022/09/16 04:41:55 by ommohame         ###   ########.fr       */
+/*   Updated: 2022/09/16 05:13:06 by ommohame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	clear_line(int sig)
 		sig_exit_code(sig);
 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
 		ft_putchar_fd('\n', 1);
+		rl_replace_line("", 1);
 		rl_on_new_line();
 	}
 }
